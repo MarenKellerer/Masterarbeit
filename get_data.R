b357b95da8bca09de78f1316ebc74e251a46c3a9
Maren Kellerer <Maren.Kellerer@gmail.com>
2022-03-07 10:29:20 +0100

    reading data

---
+++ get_data.R
@@ -0,0 +1,5 @@
+
+# read data
+wave1 <- read.csv("./Data/d_w1.csv", header = TRUE, sep = ",", fill = TRUE)
+wave2 <- read.csv("./Data/d_w2.csv", header = TRUE, sep = ",", fill = TRUE)
+wave3 <- read.csv("./Data/d_w3.csv", header = TRUE, sep = ",", fill = TRUE)
