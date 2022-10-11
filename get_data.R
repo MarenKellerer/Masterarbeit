1981ac9617a2de75e61e490ae352d7fdc32cb421
MarenKellerer <40601082+MarenKellerer@users.noreply.github.com>
2022-10-11 16:28:27 +0200

    Delete get_data.R

--- get_data.R
+++
@@ -1,5 +0,0 @@
-
-# read data
-wave1 <- read.csv("./Data/d_w1.csv", header = TRUE, sep = ",", fill = TRUE)
-wave2 <- read.csv("./Data/d_w2.csv", header = TRUE, sep = ",", fill = TRUE)
-wave3 <- read.csv("./Data/d_w3.csv", header = TRUE, sep = ",", fill = TRUE)
